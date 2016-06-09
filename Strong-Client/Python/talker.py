9dbb1e67d553e9bbdc218a78d83a4f2f22623a2e
Tommy <TommyCohn116@gmail.com>
2016-06-09 15:50:30 -0400

    These are going to be the same

---
+++ Strong-Client/Python/talker.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+# This is basically just example code
+
+import rospy
+from std_msgs.msg import String
+
+def talker():
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # The rate that messages will be sent, given in Hz
+    while not rospy.is_shutdown():
+        hello_str = "hello world %s" % rospy.get_time()
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
